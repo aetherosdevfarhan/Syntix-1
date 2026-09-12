8bd3e7101214bf88ed38173882c106cddc9258e6
aetherosdevfarhan <sbuckxs@gmail.com>
2026-09-12 21:17:29 +0600

    Create ping.js

---
+++ src/commands/ping.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Check if AETHEROS is online and responsive.'),
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setTitle('SYNTIX is online')
+      .setDescription(`Gateway latency: **${interaction.client.ws.ping}ms**`)
+      .setColor(0x5865F2);
+    await interaction.reply({ embeds: [embed] });
+  }
+};
